b10818a1034a82e10b0bbde036ca3157a5f8ca92
Kirill Bovin <skrill96@e1.ru>
2023-04-01 00:20:18 +0500

    after mistakes

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,11 +5,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 v1_router = DefaultRouter()
-v1_router.register(
-                    r'^posts/(?P<post_id>\d+)/comments',
-                    CommentViewSet,
-                    basename='comments'
-)
+v1_router.register(r'posts/(?P<post_id>\d+)/comments',
+                   CommentViewSet, basename='comments')
 v1_router.register('posts', PostViewSet, basename='posts')
 v1_router.register('groups', GroupViewSet, basename='group')
 
